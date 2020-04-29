a79b71b78847c38b39360ff4df8d75e51a4cbceb
nokozm <nokonokoxz@gmail.com>
2020-04-29 10:29:36 +0900

    [feat] Add matters api and npm modules

---
+++ laravel/app/Http/Controllers/MatterController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Matter;
+use App\Http\Requests\StoreMatter;
+
+class MatterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Matter::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMatter $request)
+    {
+        return Matter::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Matter::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreMatter $request, $id)
+    {
+        return Matter::where('id', $id)
+            ->update(['description' => $request->description]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Matter::where('id', $id)->delete();
+    }
+}
---
+++ laravel/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-card class="mx-auto">
+              <v-card-text>
+                <p class="text--primary">
+                  Hello Vuetify!!
+                </p>
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-btn>やったね！！</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
